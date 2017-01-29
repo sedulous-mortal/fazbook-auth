54f0fd614dacf98d17ca4f6a46dbecb4cab78055
Alison Stuart <hax@Alisons-MacBook-Air.local>
2017-01-28 20:59:30 -0500

    first commit

---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const authHelpers = require('../auth/auth-helpers');
+
+/* GET user profile page. */
+// add route here
+
+
+module.exports = router;
